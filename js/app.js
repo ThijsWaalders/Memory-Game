75d9524b472821ddf924ffc9250ffa5f42446ab8
Thijs Waalders <thijswaalders@gmail.com>
2018-03-28 18:08:07 +0200

    feat: css f up

--- js/app.js
+++ js/app.js
@@ -128,125 +128,119 @@ const cardsArray = [{
     'name': 'angellist',
   },
   {
-    'name': 'napster',
+    'name': 'earlybirds',
     //'img': 'img/star.png',
   },
   {
-    'name': 'bobomb',
+    'name': 'bomb',
     //'img': 'img/bobomb.png',
   },
   {
-    'name': 'mario',
+    'name': 'stack-overflow',
     //'img': 'img/mario.png',
   },
   {
-    'name': 'luigi',
+    'name': 'slack',
     //'img': 'img/luigi.png',
   },
   {
-    'name': 'peach',
+    'name': 'soundcloud',
     //'img': 'img/peach.png',
   },
   {
-    'name': '1up',
+    'name': 'drupal',
     //'img': 'img/1up.png',
   },
   {
-    'name': 'mushroom',
+    'name': 'windows',
     //'img': 'img/mushroom.png',
-  },
-  {
-    'name': 'thwomp',
-    //'img': 'img/thwomp.png',
-  },
-  {
-    'name': 'bulletbill',
-   // 'img': 'img/bulletbill.png',
-  },
-  {
-    'name': 'coin',
-   // 'img': 'img/coin.png',
-  },
-  {
-    'name': 'goomba',
-   // 'img': 'img/goomba.png',
   },
 ];
 
+const gameGrid = cardsArray
+  .concat(cardsArray)
+  .sort(() => 0.5 - Math.random());
 
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let previousTarget = null;
+let delay = 1200;
 
-
-// Grab the div with an id of root
 const game = document.getElementById('game');
-
-// Create a section with a class of grid
 const grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
-
-// Append the grid section to the game div
 game.appendChild(grid);
 
+gameGrid.forEach(item => {
+  const { name, img } = item;
 
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
 
-// For each item in the cardsArray array...
-cardsArray.forEach(item => {
-    // Create a div
-    const card = document.createElement('div');
-  
-    // Apply a card class to that div
-    card.classList.add('card');
-  
-    // Set the data-name attribute of the div to the cardsArray name
-    card.dataset.name = item.name;
-  
-    // Apply the background image of the div to the cardsArray image
-    card.style.backgroundImage = `url(${item.img})`;
-  
-//     // Append the div to the grid section
-//     grid.appendChild(card);
-//   });
+  const front = document.createElement('div');
+  front.classList.add('front');
+
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = `url(${img})`;
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
 });
 
-
-
-// Duplicate array to create a match for each card
-let gameGrid = cardsArray.concat(cardsArray);
-
-
-
-// For each item in the cardsArray array...
-cardsArray.forEach(item => {
-    // Create a div
-    const card = document.createElement('div');
-  
-    // Apply a card class to that div
-    card.classList.add('card');
-  
-    // Set the data-name attribute of the div to the cardsArray name
-    card.dataset.name = item.name;
-  
-    // Apply the background image of the div to the cardsArray image
-    card.style.backgroundImage = `url(${item.img})`;
-  
-    // Append the div to the grid section
-    grid.appendChild(card);
+const match = () => {
+  const selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.add('match');
   });
+};
 
+const resetGuesses = () => {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
 
-// Randomize game grid on each load
-gameGrid.sort(() => 0.5 - Math.random());
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.remove('selected');
+  });
+};
 
+grid.addEventListener('click', event => {
 
+  const clicked = event.target;
 
+  if (
+    clicked.nodeName === 'SECTION' ||
+    clicked === previousTarget ||
+    clicked.parentNode.classList.contains('selected')
+  ) {
+    return;
+  }
 
-// Add event listener to grid so anytime an element is clicked, the selected class will be applied to it
-grid.addEventListener('click', function (event) {
-    // The event target is our clicked item
-    let clicked = event.target;
-  
-    // Do not allow the grid section itself to be selected; only select divs inside the grid
-    if (clicked.nodeName === 'SECTION') { return; }
-    
-    // Add selected class
-    clicked.classList.add('selected');
-   });
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
+    }
+
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousTarget = clicked;
+  }
+
+});
