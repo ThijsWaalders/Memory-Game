506c71e0b6f36966eb023faaec7ad3ebda07ee73
Thijs Waalders <thijswaalders@gmail.com>
2018-04-04 09:25:51 +0200

    feat: trying to add the winning screen

--- js/app.js
+++ js/app.js
@@ -33,7 +33,21 @@ const cardsArray = [{
 },
 ];
 
-//Duplicate the array/cards to get a pair of all 8 cards
+// Shuffle function from http://stackoverflow.com/a/2450976 is a lot bigger than the randomize function I've followed:
+// function shuffle(cardsArray) {
+// var currentIndex = cardsArray.length,
+//   temporaryValue, randomIndex;
+// while (currentIndex !== 0) {
+//   randomIndex = Math.floor(Math.random() * currentIndex);
+//   currentIndex -= 1;
+//   temporaryValue = cardsArray[currentIndex];
+//   cardsArray[currentIndex] = cardsArray[randomIndex];
+//   cardsArray[randomIndex] = temporaryValue;
+// }
+// return cardsArray;
+// }
+
+//Duplicate the array/cards to get a pair of all 8 cards and then randomize all cards
 let gameGrid = cardsArray.concat(cardsArray).sort(function () {
   return 0.5 - Math.random();
 });
@@ -43,6 +57,7 @@ let secondGuess = '';
 let count = 0;
 var previousTarget = null;
 var delay = 1200;
+let matchCount = 0;
 
 // create grid
 const game = document.getElementById('game');
@@ -75,15 +90,16 @@ gameGrid.forEach(function (item) {
   card.appendChild(back);
 });
 
-// add class to style selected cards
+// add class to style selected cards when they match
 const match = function match() {
   var selected = document.querySelectorAll('.selected');
   selected.forEach(function (card) {
     card.classList.add('match');
+    matchCount = matchCount +1;
   });
 };
 
-// add selection
+// reset function after the player selected 2 cards.
 var resetGuesses = function resetGuesses() {
   firstGuess = '';
   secondGuess = '';
@@ -135,39 +151,24 @@ grid.addEventListener('click', function (event) {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// write a function to show the winning screen/modal when all 16 cards match
+// When a user wins the game, a modal appears to congratulate the player and ask if they want to play again. It should also tell the user how much time it took to win the game, and what the star rating was.
+if (matchCount === 16) {
+    // create a div for the modal screen and give it a class to style it
+    const winModal = document.createElement('div');
+    winModal.classList.add('win-screen');
+    deck.appendChild(winModal);
+    matchCount = 0;
+  }
 
 
 
 
 
 /*
-1. create an array to hold the cards
-2. change the  name of the array in the shuffle function to the list array
-3. create a loop that loops true each card and create it's html and stop at the last one?
+1. create an array to hold the cards - Check!
+2. change the  name of the array in the shuffle function to the list array - Check! (used a smaller peace of code than the given one)
+3. create a loop that loops true each card and create it's html and stop at the last one - Check!
 
 
 * Create a list that holds all of your cards
@@ -183,17 +184,17 @@ grid.addEventListener('click', function (event) {
 // Shuffle function from http://stackoverflow.com/a/2450976
 
 
-/*function shuffle(cardsList) {
-var currentIndex = cardsList.length,
+/*function shuffle(cardsArray) {
+var currentIndex = cardsArray.length,
   temporaryValue, randomIndex;
 while (currentIndex !== 0) {
   randomIndex = Math.floor(Math.random() * currentIndex);
   currentIndex -= 1;
-  temporaryValue = cardsList[currentIndex];
-  cardsList[currentIndex] = cardsList[randomIndex];
-  cardsList[randomIndex] = temporaryValue;
+  temporaryValue = cardsArray[currentIndex];
+  cardsArray[currentIndex] = cardsArray[randomIndex];
+  cardsArray[randomIndex] = temporaryValue;
 }
-return cardsList;
+return cardsArray;
 }
 */
 
