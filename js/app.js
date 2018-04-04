f92dc408d4fddc5740fcbae2771e3b68a9e2e3c1
Thijs Waalders <thijswaalders@gmail.com>
2018-04-04 17:25:31 +0200

    fix: some comments cleared

--- js/app.js
+++ js/app.js
@@ -199,15 +199,14 @@ startButton.addEventListener('click',function (){
 
 
 * Display the cards on the page - Check!
-*   - shuffle the list of cards using the provided "shuffle" method below
-*   - loop through each card and create its HTML
-*   - add each card's HTML to the page
+*   - shuffle the list of cards using the provided "shuffle" method below - Check! but I changed it because I found a compacter/shorter code
+*   - loop through each card and create its HTML - Check!
+*   - add each card's HTML to the page - Check!
+
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-
-
-/*function shuffle(cardsArray) {
+function shuffle(cardsArray) {
 var currentIndex = cardsArray.length,
   temporaryValue, randomIndex;
 while (currentIndex !== 0) {
@@ -219,13 +218,7 @@ while (currentIndex !== 0) {
 }
 return cardsArray;
 }
-*/
 
-
-//   - loop through each card and create its HTML
-
-
-/*
 * set up the event listener for a card. If a card is clicked:
 *  - display the card's symbol (put this functionality in another function that you call from this one)
 *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
